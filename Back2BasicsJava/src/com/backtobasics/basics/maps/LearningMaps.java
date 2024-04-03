89cf054538bd1a1a49cf146afc22bda6aa994c6a
Shiffli Kureish <shiffli.k@gmail.com>
2024-04-03 23:29:18 +0530

    TestCommitFromDeb12Honor

--- Back2BasicsJava/src/com/backtobasics/basics/maps/LearningMaps.java
+++ Back2BasicsJava/src/com/backtobasics/basics/maps/LearningMaps.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class LearningMaps {
     public static void main(String[] args) {
         // aHashMap();
-        linkedAccessMaps();
+        // linkedAccessMaps();
     }
     
     private static void linkedAccessMaps() {
