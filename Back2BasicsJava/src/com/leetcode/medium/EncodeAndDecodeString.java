2e2b39b19e7a3338ab65fd24c236d7eca833859a
shiffli.k_OrbisUbuntu <shiffli.k@gmail.com>
2025-07-14 00:37:05 +0530

    LeetCode 271| Encode and Decode String
    Semi Solved. One major bug is that cases where the length of String is more than 9 i.e. two or more digits will have problem.
    
    Will try this tomorrow, hopefully.

--- Back2BasicsJava/src/com/leetcode/medium/EncodeAndDecodeString.java
+++ Back2BasicsJava/src/com/leetcode/medium/EncodeAndDecodeString.java
@@ -5,26 +5,75 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
 
 public class EncodeAndDecodeString {
 
+    // LeetCode 271| Encode and Decode String
+
     public static void main(String[] args) {
         // Input: ["we","say",":","yes"]
         // Input: ["neet","code","love","you"]
-
+        System.out.println("------------");
         System.out.println(
+            "The (This could work on any language) way" + 
             decode(encode(Arrays.asList("we","say",":","yes")))
             + " \n" + 
             decode(encode(Arrays.asList("neet","code","love","you")))
         );
 
+        // System.out.println(
+        //     "The Java Serialization way!" + 
+        //     decodeSerialize(encodeSerialize(Arrays.asList("we","say",":","yes")))
+        //     + " \n" + 
+        //     decodeSerialize(encodeSerialize(Arrays.asList("neet","code","love","you")))
+        // );
 
+        
+    }
+    
+    
+    public static List<String> decode(String str) {
+
+        if(str.length() < 1)
+            return Arrays.asList("");
+
+        List<String> resultingArList = new ArrayList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            
+            if('#' == currentChar && i+1 < str.length()){
+                StringBuilder resultingWord = new StringBuilder();
+                char followingChar = str.charAt(i+1);
+                int expectedLength = Integer.parseInt(followingChar+"");
+
+                for(int j = 0 ; j < expectedLength ; j++){
+                    resultingWord.append(str.charAt(j+i+2));
+                }
+                i += expectedLength - 1;
+                resultingArList.add(resultingWord.toString());
+            }
+
+        }
+
+        return resultingArList;
     }
 
     public static String encode(List<String> strs) {
+        if(strs.size() < 1)
+            return "";
+        StringBuilder encodedVal = new StringBuilder();
+        for(String eachItem : strs)
+            encodedVal.append("#" + eachItem.length() + eachItem);
+        System.out.println("Encoded of " + strs + " is " + encodedVal.toString());
+        return encodedVal.toString();
+    }
+
+    public static String encodeSerialize(List<String> strs) {
         try {
             ByteArrayOutputStream bros = new ByteArrayOutputStream();
             ObjectOutputStream oos = new ObjectOutputStream(bros);
@@ -36,7 +85,7 @@ public class EncodeAndDecodeString {
         }
     }
     
-    public static List<String> decode(String str) {
+    public static List<String> decodeSerialize(String str) {
         try {
             ByteArrayInputStream bris = new ByteArrayInputStream(Base64.getDecoder().decode(str));
             ObjectInputStream bis = new ObjectInputStream(bris);
