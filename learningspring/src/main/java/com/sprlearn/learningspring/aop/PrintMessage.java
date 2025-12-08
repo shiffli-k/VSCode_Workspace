0abddd20df73b3f73f3119bf721a5828790bea11
shiffli.k_OrbisUbuntu <shiffli.k@gmail.com>
2025-12-08 09:28:47 +0530

    Commiting before I wipe my PC clean

--- learningspring/src/main/java/com/sprlearn/learningspring/aop/PrintMessage.java
+++ learningspring/src/main/java/com/sprlearn/learningspring/aop/PrintMessage.java
@@ -1,13 +1,38 @@
 package com.sprlearn.learningspring.aop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+
 @RestController
 public class PrintMessage {
+
+    private static final Logger log = LoggerFactory.getLogger(PrintMessage.class);
+
+    @PostConstruct
+    private void onCreation(){
+        log.info("API Controller Ready!");
+    }
+
+    @PreDestroy
+    private void onDestroy(){
+        log.info("Shutting down API Controller");
+    }
+
+    @GetMapping("/")
+    public String basePath() {
+        log.debug("Health check Request");
+        return "Up and Running!";
+    }
+    
 
     @GetMapping("/welcome")
     public ResponseEntity<String> print() {
