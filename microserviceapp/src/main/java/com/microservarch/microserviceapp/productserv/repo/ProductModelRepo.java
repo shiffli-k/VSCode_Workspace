ca20683f956dc043c54c286269cfe415bea05b33
ShiffliOnMac <shiffli.k@gmail.com>
2026-07-10 21:06:56 +0530

    Setting up forMicroServices

--- microserviceapp/src/main/java/com/microservarch/microserviceapp/productserv/repo/ProductModelRepo.java
+++
@@ -1,8 +0,0 @@
-package com.microservarch.microserviceapp.productserv.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.microservarch.microserviceapp.productserv.model.ProductModel;
-
-public interface ProductModelRepo extends JpaRepository<ProductModel, Long>{
-}
