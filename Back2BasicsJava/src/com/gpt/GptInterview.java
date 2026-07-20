151d0bcda7e18c4abb5445c427237d530a23f78d
ShiffliOnMac <shiffli.k@gmail.com>
2026-07-20 10:04:14 +0530

    Setting up basic Jenkinsfile

--- Back2BasicsJava/src/com/gpt/GptInterview.java
+++ Back2BasicsJava/src/com/gpt/GptInterview.java
@@ -1,32 +1,5 @@
 package com.gpt;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-
 public class GptInterview {
-    public static void main(String[] args) {
-    
-        List<Integer> intList = new ArrayList<>();
-        Set<Integer> intSet = new HashSet<>();
-        Integer i1 = Integer.valueOf(1);
-        Integer i2 = Integer.valueOf(2);
-        Integer i3 = Integer.valueOf(1);
 
-        intSet.add(i1);
-        intSet.add(i2);
-        intSet.add(i3);
-
-        System.out.println(
-            intSet
-        );
-    
-    }
 }
