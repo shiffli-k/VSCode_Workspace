f069d72f09f73eeadba94da8685ad99e7fe44147
shiffliWslWrk <shiffli.k@gmail.com>
2026-08-13 13:12:43 +0530

    Setting up projects for learning resilience

---
+++ microserviceapp/resilience/servicemain/src/main/java/com/resilience/simulate/servicemain/ServicemainApplication.java
@@ -0,0 +1,13 @@
+package com.resilience.simulate.servicemain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicemainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicemainApplication.class, args);
+	}
+
+}
---
+++ microserviceapp/resilience/servicemain/src/test/java/com/resilience/simulate/servicemain/ServicemainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resilience.simulate.servicemain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicemainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
